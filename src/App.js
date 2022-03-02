3ba463bf6719610ed28d9e039e612a79279b0160
BrynnaWee <bobae.we@gmail.com>
2022-03-03 07:12:19 +0900

    state까지 정리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
 
 function App() {
   return (
-    <MyComponent city={'서울'}>보석이</MyComponent>
+    // <MyComponent city={'서울'}>보석이</MyComponent>
+    // <Counter />
+    <Say />
   );
 }
 
