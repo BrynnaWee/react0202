27b615b7ad382e924bc2462fe325834fe563ac79
BrynnaWee <bobae.we@gmail.com>
2022-03-13 00:46:45 +0900

    ch7.라이프사이클(2)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
@@ -7,20 +8,49 @@ import './App.css';
 // import ValidationSample from './ValidationSample';
 // import IterationSample from './IterationSample';
 import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
-function App() {
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+
+class App extends React.Component {
+
+state = {
+  color : '#000000'
+}
+
+render(){
   return (
-    <>
-    {/* // <MyComponent city={'서울'}>보석이</MyComponent>
-    // <Counter />
-    // <Say />
-    // <EventPractice /> */}
-    {/* <EventPracticeFunc />
-    <ValidationSample /> */}
-    {/* <IterationSample /> */}
-    <LifeCycleSample />
-    </>
-  );
+    // // <MyComponent city={'서울'}>보석이</MyComponent>
+    // // <Counter />
+    // // <Say />
+    // // <EventPractice />
+    // <EventPracticeFunc />
+    // <ValidationSample /> 
+    // <IterationSample />
+
+    <div>
+    <button onClick={this.handleClick}>랜덤색상</button>
+
+      <ErrorBoundary>
+        <LifeCycleSample 
+          color = { this.state.color }
+        />
+      </ErrorBoundary>
+    </div>
+
+  );}
+/**
+ * functions
+ */
+
+handleClick = () => {
+  this.setState({ color : getRandomColor() });
+}
+
 }
 
 export default App;
